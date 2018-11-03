7c12a2febf8fe2f3400729466e821cc0033cff79
Thomas Herzog <herzog.thomas81@gmail.com>
2018-11-03 08:54:56 +0100

    - Moved all sources to proper camel namespace - Replaced apache license headers with ASF ones

--- src/main/java/org/apache/camel/component/smbj/SmbFileOperations.java
+++ src/main/java/org/apache/camel/component/smbj/SmbFileOperations.java
@@ -1,19 +1,20 @@
-/*****************************************************************
- Copyright 2015-2018 the original author or authors.
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- *****************************************************************/
-package at.ihet.camel.extras.smbj;
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.smbj;
 
 
 import com.hierynomus.msdtyp.AccessMask;
@@ -445,8 +446,7 @@ public class SmbFileOperations implements GenericFileOperations<SmbFile> {
      * Opens a read only file from the smb share
      *
      * @param share the disk share to open the file from
-     * @param name  the fully qualified file name to open
-     * @return the opened file
+     * @param name  the fully qualified file name to open     * @return the opened file
      * @throws com.hierynomus.mssmb2.SMBApiException if the file cannot be opened
      */
     private DiskEntry openReadOnlyFile(final DiskShare share,
